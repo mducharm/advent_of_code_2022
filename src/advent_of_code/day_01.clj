13d56f55b66d6bd8ef1c5654e4f5c470ab837ee1
Michael Ducharm <ducharmdev@gmail.com>
2022-12-01 17:02:56 -0500

    day 1 solutions

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,29 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require
+   [clojure.string :as str]))
+
+(defn sum [nums]
+  (reduce + (map #(Integer/parseInt %) nums)))
+
+(defn get-sums [input]
+  (->> input
+       (str/split-lines)
+       (partition-by str/blank?)
+       (filter (partial not= '("")))
+       (map sum)))
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (->> input
+       (get-sums)
+       (apply max)))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (->> input
+       (get-sums)
+       (sort)
+       (take-last 3)
+       (reduce +)))
