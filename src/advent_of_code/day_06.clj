3cf2b2e35c15ed3dccc72f07a8fb6ded6e3ffd17
Michael Ducharm <ducharmdev@gmail.com>
2022-12-06 07:25:06 -0500

    day 6 solutions

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,11 +1,20 @@
 (ns advent-of-code.day-06)
 
+(defn get-answer [input window-size]
+  (->> input
+       (partition window-size 1)
+       (map distinct)
+       (map count)
+       (take-while (partial not= window-size))
+       (count)
+       (+ window-size)))
+
 (defn part-1
   "Day 06 Part 1"
   [input]
-  input)
+  (get-answer input 4))
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (get-answer input 14))
