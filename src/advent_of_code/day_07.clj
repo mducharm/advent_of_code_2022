9535608353dca3ea00b4a9d29c629e56bff1825a
Michael Ducharm <ducharmdev@gmail.com>
2022-12-08 18:17:49 -0500

    further attempts....

--- src/advent_of_code/day_07.clj
+++ src/advent_of_code/day_07.clj
@@ -6,53 +6,40 @@
 (def input (slurp (clojure.java.io/resource "day-07.txt")))
 (str/split-lines input)
 
-(defn contains-cd? [cmd] (str/includes? cmd "$ cd"))
+(defn determine-cmd-type [cmd]
+  (cond
+    (str/includes? cmd "$ cd ..") :up-dir
+    (str/includes? cmd "$ cd") :cd
+    (not (nil? (re-seq #"\d+" cmd))) :file
+    :else :no-op))
 
-(defn determine-path [current-path cmds]
-  (letfn [(update [current-path cd]
-            (if (str/includes? cd "cd ..")
-              (into [] (drop-last current-path))
-              (conj current-path (subs cd 5))))]
-    (reduce update current-path cmds)))
+(defn cmd-is? [cmd-type cmd]
+  (= cmd-type (determine-cmd-type cmd)))
 
-;; (determine-path (list) (list "$ cd /"))
-;; (determine-path ["/" "a"] (list "$ cd e"))
-;; (determine-path ["/" "a" "e"] (list "$ cd .." "$ cd .." "$ cd d"))
+(defn cmd-is-not? [cmd-type cmd]
+  (not= cmd-type (determine-cmd-type cmd)))
 
-(defn process-paths [commands]
-  (letfn [(update [[current-path acc] current-cmd]
-            (if (some contains-cd? current-cmd)
-              ;; cd, convert to path
-              (let [new-path (determine-path current-path current-cmd)]
-                [new-path (conj acc new-path)])
-              ;; ls, leave for now
-              [current-path (conj acc current-cmd)]))]
-    (reduce update [[] []] commands)))
+(defn chunk-by-file [coll]
+  (let [last-path (atom ())]
+    (partition-by #(if (or (string? %) (= :up-dir %))
+                     (reset! last-path %)
+                     @last-path) coll)))
 
-(defn calc-immediate-folder-sum [files]
-  (->> files
-       (map (partial re-seq #"\d+"))
-       (reduce concat)
-       (map read-string)
-       (reduce +)))
-
+(defn parse-input [input]
+  (->> input
+       (str/split-lines)
+       (filter (partial cmd-is-not? :no-op))
+       (map #(case (determine-cmd-type %)
+               :cd (subs % 5)
+               :file (read-string (re-find #"\d+" %))
+               :up-dir :up-dir
+               :else :no-op))))
 
 (defn part-1
   "Day 07 Part 1"
   [input]
   (->> input
-       (str/split-lines)
-       (partition-by contains-cd?)
-       (process-paths)
-       (second)
-       (partition 2)
-       ;; join path into string
-       (map #(cons (str/join (first %)) (rest %)))
-       ;; find initial sums of each dir
-       (map #(list (first %) (calc-immediate-folder-sum (second %))))
-       (reduce concat)
-       (apply hash-map))
-)
+       (parse-input)))
 
 
 (defn part-2
